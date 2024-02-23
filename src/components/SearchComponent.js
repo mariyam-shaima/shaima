91d2424f47d006f16052b5d6949b5920432b5a74
mariyam-shaima <161009552+mariyam-shaima@users.noreply.github.com>
2024-02-23 13:31:53 +0530

    Add files via upload

---
+++ src/components/SearchComponent.js
@@ -0,0 +1,34 @@
+import React from 'react';
+ 
+
+function SearchComponent({ searchCourse, courseSearchUserFunction }) {
+
+    return (
+
+        <header className="App-header">
+
+            <h1>Shopping Cart</h1>
+
+            <div className="search-bar">
+
+                <input
+
+                    type="text"
+
+                    placeholder="Search for Products..."
+
+                    value={searchCourse}
+
+                    onChange={courseSearchUserFunction}
+
+                />
+
+            </div>
+
+        </header>
+
+    );
+}
+ 
+
+export default SearchComponent;
